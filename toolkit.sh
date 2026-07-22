f1fb94a9ef5cf7e15da550ae7315574e0259661c
Kedarini <kedardamps@gmail.com>
2026-07-22 14:59:28 +0200

    add handling of memory command

--- toolkit.sh
+++ toolkit.sh
@@ -5,6 +5,7 @@ if [ $# -eq 0 ]; then
     echo "Usage: ./toolkit.sh <argument>"
     echo "Possible arguments:"
     echo "> archive <directory> - Create an archive with logs from your pc default log catalog"
+    echo "> memory - Show you used / total disk memory and RAM"
 fi
 
 if [ "$1" == "archive" ]; then
@@ -16,4 +17,9 @@ if [ "$1" == "archive" ]; then
         echo "Archive require argument"
         echo "Usage: ./toolkit.sh argument <directory>"
     fi
+fi
+
+if [ "$1" == "memory" ]; then
+    source "commands/memory-usage.sh"
+    memory-monitor
 fi
