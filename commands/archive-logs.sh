6c7024e7f6e288fcc43fb568beb95d12b0be6330
Kedarini <kedardamps@gmail.com>
2026-07-22 14:42:02 +0200

    script to archive logs from system and save at given directory

---
+++ commands/archive-logs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+archive_logs() {
+    filename="logs_archive_$(date '+%Y%m%d_%H%M%S').tar.gz"
+    tar -czf "$filename" /var/log 2>/dev/null
+    mv "$filename" "$1"
+
+    echo "Successfully archived logs in $1"
+}
